42ad8f916457ba0c78b425548018faf0dff9d69f
adf <asdf@adsf.com>
2016-06-29 11:53:36 +0545

    static resouces, dynamic resources,local resources ,window level resources ,application resources

--- Styles_Resources_Templates/MainWindow.xaml.cs
+++ Styles_Resources_Templates/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Styles_Resources_Templates
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
